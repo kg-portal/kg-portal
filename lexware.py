4378a78d0fb3dbd019302eacac51791f1b96401c
kg-portal <info@kg-store.de>
2026-03-30 15:59:01 +0200

    Update lexware.py

--- lexware.py
+++ lexware.py
@@ -15,6 +15,9 @@ def sync_lexware_to_db():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
+    cursor.execute("DELETE FROM lexware_cache")
+    conn.commit()
+
     try:
         page = 0
         invoices = []
